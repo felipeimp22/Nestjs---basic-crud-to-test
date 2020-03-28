ac740346d1d8902686de3e5a0a2623bf3ce494bf
felipeimp22 <felipeimperio.imperioo@gmail.com>
2020-03-27 21:36:27 -0300

    exclude files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 // import { TasksController } from './tasks/tasks.controller';
 // import { TasksService } from './tasks/tasks.service';
 import { TasksModule } from './tasks/tasks.module';
@@ -9,7 +7,5 @@ import {MongooseModule} from '@nestjs/mongoose'
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/api',{useNewUrlParser:true,useUnifiedTopology: true}),
     TasksModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
